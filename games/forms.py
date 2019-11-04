e3277f3130b6e47dc4ed5ecf202fbdf8ef766a4f
Vinicius Ribeiro <viniciusribeiro@ua.pt>
2019-11-04 23:47:34 +0000

    search funcionando

--- games/forms.py
+++ games/forms.py
@@ -3,3 +3,11 @@ from django import forms
 
 class SearchGame(forms.Form):
     search = forms.CharField(label='Search game', max_length=100)
+
+
+class SearchParam(forms.Form):
+    # league_id = forms.CharField(label='Search league', max_length=100)
+    # country_id = forms.CharField(label='Search country', max_length=100)
+    country_name = forms.CharField(label='Search Country name', max_length=100)
+    _from = forms.CharField(label='Search _from', max_length=100, required=False)
+    to = forms.CharField(label='Search to', max_length=100)
--- games/views.py
+++ games/views.py
@@ -1,11 +1,14 @@
+import json
+
 from django.forms import forms
 from django.shortcuts import redirect, render
 from django.views.generic import TemplateView, View, ListView
 from django.db.models import Q
 
-from games.forms import SearchGame
+# from games.forms import SearchGame
+from games.forms import *
 from games.models import Game, make_request
-import datetime
+from datetime import date
 
 
 class GameView(forms.Form, TemplateView, View):
@@ -21,17 +24,40 @@ class GameView(forms.Form, TemplateView, View):
 
 
 class SearchView(forms.Form, TemplateView, View):
-    search = SearchGame
     template_name = 'search_results.html'
+    search = SearchParam
+    json_data = {"Brazil": ("19", "68"), "Portugal": ("115", "391"), "England": ("41", "148")}
 
     def post(self, request, *args, **kwargs):
         form = self.search(data=request.POST)
+        # if not form.cleaned_data['_from']:
+        #     _from = str(date.today())
+        # else:
+        #     if form.is_valid():
+        #         _from = form.cleaned_data['_from']
         if form.is_valid():
-            query = form.cleaned_data['query']
-            games = Game.objects.filter(match_hometeam_name__icontains=query)
-            return self.render_to_response({'games': games, 'query': query})
-        return self.render_to_response({'form': form})
+            # league_id = form.cleaned_data['league_id']
+            # country_id = form.cleaned_data['country_name']
+            _from = form.cleaned_data['_from']
+            # if not _from:
+            #     _from = date.today()
+            # else:
+            #     _from = form.cleaned_data['_from']
+            to = form.cleaned_data['to']
+            country_id, league_id = self.json_data[form.cleaned_data['country_name']]
+            api_get = make_request(
+                'https://apiv2.apifootball.com/?action'
+                '=get_predictions'
+                '&from={}'
+                '&to={}'
+                '&APIkey=141b8a53928078cd692ab00bbba24a1ec56e82d986c1decba0875b11147949c9'
+                '&country_id={}'
+                '&league_id={}'.format(_from, to, country_id, league_id))
+            search = {
+                'games': api_get
+            }
+            return self.render_to_response(search)
 
     def get(self, request, *args, **kwargs):
-        form = SearchGame()
+        form = SearchParam()
         return self.render_to_response({'form': form})
