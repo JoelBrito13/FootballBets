19d8de9e8d08d318582547c4d7b84472b82388fd
Vinicius Ribeiro <viniciusribeiro@ua.pt>
2019-11-05 13:30:34 +0000

    games

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,15 @@
 from django.contrib.auth.forms import UserCreationForm
 
 from users.models import Person
+from django import forms
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = Person
         fields = ('username', 'balance')
+
+
+class LoadCreditsForm(forms.Form):
+    insert_credit = forms.CharField(max_length=100, label=' Insert Credits', required=False)
+    withdraw_credit = forms.CharField(max_length=100, label='Withdraw Credits', required=False)
